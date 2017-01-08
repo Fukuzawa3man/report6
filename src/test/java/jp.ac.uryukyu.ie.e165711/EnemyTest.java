6c0dc6e124ed46e9032f35e505d5cda22b7bd8d6
Wako Oshiro <e165711@ie.u-ryukyu.ac.jp>
2017-01-08 19:32:25 +0900

    Report6_Step2_first commit

---
+++ src/test/java/jp.ac.uryukyu.ie.e165711/EnemyTest.java
@@ -0,0 +1,25 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by e165711 on 2016/11/26.
+ */
+public class EnemyTest {
+    /**
+     * 「死んでる状態では攻撃できない」ことを確認.
+     * @throws Exception
+     */
+    @Test
+    public void attack() throws Exception {
+        int heroHP = 10;
+        Hero hero = new Hero("テスト勇者", heroHP, 5);
+        Enemy enemy = new Enemy("テストスライム", 6, 3);
+        enemy.setIsDead(true);
+        for(int i=0; i<10; i++) {
+            enemy.attack(hero); //乱数で0ダメージとなることもあるため、複数回実行してみる。
+        }
+        assertEquals(heroHP, hero.getHitPoint());
+    }
+
+}
