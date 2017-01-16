88177885b67ce696b2aec5817a6852a18dcbe98f
Wako Oshiro <e165711@ie.u-ryukyu.ac.jp>
2017-01-16 21:11:37 +0900

    Report6_Step3_2 commit

--- src/main/java/jp.ac.uryukyu.ie.e165711/Enemy.java
+++ src/main/java/jp.ac.uryukyu.ie.e165711/Enemy.java
@@ -1,3 +1,4 @@
+package jp.ac.uryukyu.ie.e165711;
 /**
  *  敵クラス。
  *  String name; //敵の名前
@@ -29,13 +30,15 @@ public class Enemy extends LivingThing {
             if(damage == 0){
                 System.out.printf("%sの攻撃！,,,だが，%sは攻撃を回避した！！\n", getName(), opponent.getName());
             }else {
-                if (rate <= 30) {
+                if (rate <= 3) {
+                    damage = damage * 2;
                     System.out.printf("%sの攻撃！痛恨の一撃！！%sに%dのダメージを与えた！！\n", getName(), opponent.getName(), damage);
                 } else {
                     System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", getName(), opponent.getName(), damage);
                 }
+                opponent.wounded(damage);
             }
-            opponent.wounded(damage);
+
         }
 
     }
